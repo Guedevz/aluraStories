0f7cacb19347b349d5adfce33dcc3f629b620cb0
Marianna Guedez <mguedez.dev@gmail.com>
2025-01-14 17:25:56 -0300

    Instala la dependencia de react-router-dom. Encansula las rutas de las paginas

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,59 @@
+// Router.......................
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 
+// Components...................
+
+
+// Pages.........................
+import Home from '../Home'
+import Authors from '../Authors'
+import AuthorPosts from '../AuthorPosts'
+import CategoryPosts from '../CategoryPosts'
+import CreatePosts from '../CreatePosts'
+import Dashboard from '../Dashboard'
+import DeletePost from '../DeletePost'
+import EditPost from '../EditPost'
+import Favorites from '../Favorites'
+import Logout from '../Logout'
+import MyAccount from '../MyAccount'
+import NotFound from '../NotFound'
+import PostDetail from '../PostDetail'
+import Register from '../Register'
+import SignIn from '../SignIn'
+import UserProfile from '../UserProfile'
+
+
+// Styles.........................
 import './App.css'
 
+const AppRoutes = () => {
+  let routes = useRoutes ([
+    { path: '/', element: <Home /> },
+    { path: '/authors', element: <Authors /> },
+    { path: '/posts/user/:id', element: <AuthorPosts /> },
+    { path: '/posts/categories/:category', element: <CategoryPosts /> },
+    { path: '/create-posts', element: <CreatePosts /> },
+    { path: '/my-posts/:id', element: <Dashboard /> },
+    { path: '/posts/:id/delete', element: <DeletePost /> },
+    { path: '/posts/:id/edit', element: <EditPost /> },
+    { path: '/favorites', element: <Favorites /> },
+    { path: '/logout', element: <Logout /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/posts/:id', element: <PostDetail /> },
+    { path: '/register', element: <Register /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/profile/:id', element: <UserProfile /> },
+    { path: '/*', element: <NotFound /> },
+  ])
+
+  return routes
+}
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
