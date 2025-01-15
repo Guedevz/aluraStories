63168901a15ff635e05aafd71e94973de66ba44e
Marianna Guedez <mguedez.dev@gmail.com>
2025-01-14 21:52:05 -0300

    Crea el archivo y estructura del componente Card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,21 @@
+
+
+const Card = () => {
+    return (
+        <div>
+            <figure>
+                <span></span>
+                <img src="" alt="" />
+                <div>❤️</div>
+            </figure>
+            <h2>title</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti omnis harum similique, nulla veniam animi necessitatibus alias minima laboriosam eos at dolorum, tenetur facilis sed. Ipsum culpa sunt a debitis?</p>
+            <p>
+                <span>14 JANUARY</span>
+                <span>3K VIEWS</span>
+            </p>
+        </div>
+    )
+}
+
+export default Card
