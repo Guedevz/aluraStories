f8b3e068bd8d10b22a3d5b8d791fc1855924fd6f
Marianna Guedez <mguedez.dev@gmail.com>
2025-01-16 12:01:53 -0300

    Arregle la estructura de los links del componente

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,63 +1,70 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const Filters = () => {
-
-    const activeStyle = 'underline underline-offset-8 font-medium text-indigo-700 text-2xl'
+    const activeStyle = 'underline underline-offset-8 font-medium text-indigo-700 text-2xl';
 
     return (
         <nav className='flex justify-center items-center w-full py-24 px-12 text-gray-500'>
             <ul className='flex flex-wrap justify-center gap-12'>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:All' className={({ isActive}) => isActive ? activeStyle : undefined }
-
+                    <NavLink
+                        to='/posts/categories/All'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
-                    </NavLink>
                         All
+                    </NavLink>
                 </li>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:Web-Development' className={({ isActive}) => isActive ? activeStyle : undefined }
-                
+                    <NavLink
+                        to='/posts/categories/Web-Development'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
-                    </NavLink>
                         Web Development
+                    </NavLink>
                 </li>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:Data-Science' className={({ isActive}) => isActive ? activeStyle : undefined }
-                
+                    <NavLink 
+                        to='/posts/categories/Data-Science'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
-                    </NavLink>
                         Data Science
+                    </NavLink>
                 </li>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:Marketing' className={({ isActive}) => isActive ? activeStyle : undefined }
-                
+                    <NavLink 
+                        to='/posts/categories/Marketing'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
-                    </NavLink>
                         Marketing
+                    </NavLink>
                 </li>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:Digital-Content' className={({ isActive}) => isActive ? activeStyle : undefined }
-                
+                    <NavLink
+                        to='/posts/categories/Digital-Content'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
-                    </NavLink>
                         Digital Content
+                    </NavLink>
                 </li>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:Blockchain-Web3' className={({ isActive}) => isActive ? activeStyle : undefined }
-                
+                    <NavLink
+                        to='/posts/categories/Blockchain-Web3'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
-                    </NavLink>
                         Blockchain and Web3
+                    </NavLink>
                 </li>
                 <li className='whitespace-nowrap'>
-                    <NavLink to='/posts/categories/:Cybersecurity' className={({ isActive}) => isActive ? activeStyle : undefined }
-                
+                    <NavLink
+                        to='/posts/categories/Cybersecurity'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                     >
+                        Cybersecurity
                     </NavLink>
-                        Cybersecurity</li>
+                </li>
             </ul>
         </nav>
-    )
-}
+    );
+};
 
-export default Filters
+export default Filters;
