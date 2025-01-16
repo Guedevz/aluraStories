680754e52ecf4c5fe5d63d7d970f7c53a3a27d81
Marianna Guedez <mguedez.dev@gmail.com>
2025-01-16 11:40:34 -0300

    Aplica los estilos al componente filters. Importa el componente en la pagina de Home

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,15 +1,60 @@
 import { NavLink } from "react-router-dom"
 
 const Filters = () => {
+
+    const activeStyle = 'underline underline-offset-8 font-medium text-indigo-700 text-2xl'
+
     return (
-        <nav>
-            <ul>
-                <li><NavLink to='/posts/categories/:Web-Development'></NavLink>Web Development</li>
-                <li><NavLink to='/posts/categories/:Data-Science'></NavLink>Data Science</li>
-                <li><NavLink to='/posts/categories/:Marketing'></NavLink>Marketing</li>
-                <li><NavLink to='/posts/categories/:Digital-Content'></NavLink>Digital Content</li>
-                <li><NavLink to='/posts/categories/:Blockchain-Web3'></NavLink>Blockchain and Web3</li>
-                <li><NavLink to='/posts/categories/:Cybersecurity'></NavLink>Cybersecurity</li>
+        <nav className='flex justify-center items-center w-full py-24 px-12 text-gray-500'>
+            <ul className='flex flex-wrap justify-center gap-12'>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:All' className={({ isActive}) => isActive ? activeStyle : undefined }
+
+                    >
+                    </NavLink>
+                        All
+                </li>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:Web-Development' className={({ isActive}) => isActive ? activeStyle : undefined }
+                
+                    >
+                    </NavLink>
+                        Web Development
+                </li>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:Data-Science' className={({ isActive}) => isActive ? activeStyle : undefined }
+                
+                    >
+                    </NavLink>
+                        Data Science
+                </li>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:Marketing' className={({ isActive}) => isActive ? activeStyle : undefined }
+                
+                    >
+                    </NavLink>
+                        Marketing
+                </li>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:Digital-Content' className={({ isActive}) => isActive ? activeStyle : undefined }
+                
+                    >
+                    </NavLink>
+                        Digital Content
+                </li>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:Blockchain-Web3' className={({ isActive}) => isActive ? activeStyle : undefined }
+                
+                    >
+                    </NavLink>
+                        Blockchain and Web3
+                </li>
+                <li className='whitespace-nowrap'>
+                    <NavLink to='/posts/categories/:Cybersecurity' className={({ isActive}) => isActive ? activeStyle : undefined }
+                
+                    >
+                    </NavLink>
+                        Cybersecurity</li>
             </ul>
         </nav>
     )
