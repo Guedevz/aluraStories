3601d42b410a073122c67d48ec6bea7efa76ff3d
Marianna Guedez <mguedez.dev@gmail.com>
2025-01-17 16:20:41 -0300

    Agrega los estilos de la informacion de los elementos renderizados del componente PostDetail

--- src/components/PostDetail/index.jsx
+++ src/components/PostDetail/index.jsx
@@ -9,25 +9,27 @@ const PostDetail = () => {
 
     return (
         <aside className={`${context.isPostDetailOpen ? 'flex' : 'hidden'}
-        w-[360px] top-[85px] h-[calc(100vh-85px)] flex-col fixed right-0 border border-black bg-indigo-100`}>
-            <div className='flex justify-between items-center p-8'>
-                <div className='flex gap-6'>
-                    <button className='py-4 px-10 text-2xl rounded-lg text-white  bg-indigo-400'>Edit</button>
-                    <button className='py-4 px-10 text-2xl rounded-lg text-white  bg-red-500'>Delete</button>
+        fixed inset-0 z-40 items-center justify-center bg-black/50 backdrop-blur-sm`}>
+            <div className='relative w-full md:w-1/2 flex flex-col gap-12 bg-white rounded-lg shadow-lg p-8 overflow-y-auto' style={{ maxHeight: "90vh" }} >
+                <div className='flex justify-between items-center pb-8'>
+                    <div className='flex gap-6'>
+                        <button className='py-4 px-10 text-2xl rounded-lg text-white  bg-indigo-400'>Edit</button>
+                        <button className='py-4 px-10 text-2xl rounded-lg text-white  bg-red-500'>Delete</button>
+                    </div>
+                        <button className='p-2 bg-indigo-500 text-3xl text-white rounded-full'>
+                            <IoClose />
+                        </button>
                 </div>
-                    <button className='p-2 bg-indigo-500 text-3xl text-white rounded-full'>
-                        <IoClose />
-                    </button>
+                <figure>
+                    <img className='w-full h-full rounded-lg' src={context.postToShow.image} alt="image" />
+                </figure>
+                <p style={{ fontSize: "clamp(2rem, 4vw, 3rem)", fontWeight: "bold"}}>{context.postToShow.title}</p>
+                <p className='flex gap-8 text-gray-400 font-medium'>
+                    <span>{context.postToShow.date}</span>
+                    <span>{context.postToShow.read}</span>
+                </p>
+                <p className='text-gray-700'>{context.postToShow.info}</p>
             </div>
-            <figure>
-                <img className='w-full h-full' src={context.postToShow.image} alt="image" />
-            </figure>
-            <p>{context.postToShow.title}</p>
-            <p>
-                <span>{context.postToShow.date}</span>
-                <span>{context.postToShow.read}</span>
-            </p>
-            <p>{context.postToShow.info}</p>
         </aside>
 
     )
