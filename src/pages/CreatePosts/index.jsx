7313048443befa4b8b1a9637ec57caa453b95e39
Marianna Guedez <mguedez.dev@gmail.com>
2025-01-20 10:44:50 -0300

    Crea y estiliza el titulo y subtitulo del componente

--- src/pages/CreatePosts/index.jsx
+++ src/pages/CreatePosts/index.jsx
@@ -4,7 +4,12 @@ import Layout from "../../components/Layout"
 const CreatePosts = () => {
     return (
         <Layout>
-            CreatePosts
+            <div className='flex flex-col justify-center items-center gap-6 px-5 text-center'>
+                <h2 style={{ fontSize: "clamp(2rem, 4vw, 3rem)", fontWeight: "bold"}}>Create your new post!</h2>
+                <p style={{ fontSize: "clamp(1.2rem, 4vw, 1.8rem)", fontWeight: "bold"}} className='text-3xl font-medium text-gray-500'>
+                    Explore the future, share your innovation—your next big tech post is here!.
+                </p>
+            </div>
         </Layout>
     )
 }
